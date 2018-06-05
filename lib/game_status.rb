6384cbfa85cd4b8e9a2c4da2ead08980814a8555
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-05 22:58:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
       position_2 = board(win_index_2)
       position_3 = board(win_index_3)
 
-      position_1 == position_2 && position_2 == position_3 && position_taken(board, win_index_1)
+      position_1 == position_2 && position_2 == position_3 && position_taken(board, index)
     end
   end
 end
