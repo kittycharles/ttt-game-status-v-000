0c6befcfe61f10300c1e5b865d7d8eeb45ddcb26
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-05 22:38:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,31 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6],
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |winning_combo|
+    if check_winning_combination?(board, 'X', winning_combo)
+      return winning_combo
+    elsif check_winning_combination?(board, 'O', winning_combo)
+      return winning_combo
+    else
+      return false
+    end
+  end
+end
+
+def check_winning_combination?(board, player, winning_combo)
+  winning_combo.all? do |position|
+    board[position] == player
+  end
+end
